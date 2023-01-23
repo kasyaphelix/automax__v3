b8405c58a6c632b50f02b332bdca31313c9bcc6c
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2023-01-23 15:31:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
@@ -28,14 +28,13 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Media Search Bot or ypu can call me as Auto-Filter Bot**
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
+🤩 **Hi {}  , I'm [ᴏɴᴀɪʀ_ғɪʟᴛᴇʀᵇᵒᵗ](https://t.me/On_air_Filter_bot) you can call me as Auto-Filter Bot**
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 IMDB_TEMPLATE = "<b><a href={url}>{title}</a>🤺ɪᴍᴅʙ</b>\n\n <b>‌‌‌‌╔‎/yᴇᴀʀ: {year}\n ╠|ʀᴀᴛɪɴɢ‌‌‌‌‎: {rating}/10‌‌‌‌ \n ╚\ɢᴇɴʀᴇ: #{genres}</b> \n\n     <b>[𝚐𝚛𝚙 1](https://t.me/+PBGW_EV3ldY5YjJl)↮[𝚐𝚛𝚙 2](https://t.me/+eDjzTT2Ua6kwMTI1)</b>"
 IMDB_TEMPLATEE = "🎬𝙽𝙰𝙼𝙴: {title} {year}\n 🤵‍♂️𝙳𝙸𝚁𝙴𝙲𝚃𝙾𝚁: #{director}\n 📝𝚆𝚁𝙸𝚃𝙴𝚁: #{writer}\n 👥ᴄᴀꜱᴛ: #{cast}"
 
-FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "⭕️𝙲𝚑𝚊𝚗𝚗𝚎𝚕 @on_air_movies👇𝙼𝚘𝚟𝚒𝚎 searching 𝚐𝚛𝚘𝚞𝚙👇")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
 if FILE_CAPTION.strip() == "":
     CUSTOM_FILE_CAPTION=None
